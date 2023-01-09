f402f2da94f3ff1fa3257eb24a990245d692ea6a
Cameron McLeod <cameronmcleod@Camerons-Air.hitronhub.home>
2023-01-08 20:16:06 -0600

    added a view to display each entry page

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 
 from . import util
 
+import markdown
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry_page(request, title):
+    name = title + '.md'
+    # remember to make a case for when this file doesnt exist
+    with open(name, 'r') as f:
+        text = f.read()
+        html = markdown.markdown(text)
+    
+    return render(request, "encyclopedia/entry_page.html", {"entry_html": html})
