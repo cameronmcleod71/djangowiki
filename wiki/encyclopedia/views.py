a08e6ed57e435a127432f40b9861cdec83d7db47
Cameron McLeod <cameronmcleod@Camerons-Air.gateway.sasktel.net>
2023-01-09 11:25:16 -0600

    Moved entires folder to inside encyc, fixed a few bugs to get a working version

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -11,10 +11,9 @@ def index(request):
     })
 
 def entry_page(request, title):
-    name = title + '.md'
-    # remember to make a case for when this file doesnt exist
-    with open(name, 'r') as f:
-        text = f.read()
-        html = markdown.markdown(text)
+
+    # TODO: If an entry is requested that does not exist, the user should be presented with an error page indicating that their requested page was not found.
+    
+    html = markdown.markdown(util.get_entry(title))
     
     return render(request, "encyclopedia/entry_page.html", {"entry_html": html})
