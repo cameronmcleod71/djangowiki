af90e30a038840040657366d9fc5ce618e6461d5
Cameron McLeod <cameronmcleod@Camerons-Air.gateway.sasktel.net>
2023-01-10 11:59:04 -0600

    added the random button which will give the client a random entry page

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("<str:title>", views.entry_page, name="entry"),
     path("search/", views.search, name= "search"),
-    path("create/", views.create_page, name="create")
-
+    path("create/", views.create_page, name="create"),
+    path("random/", views.random_page, name="random"),
+    path("edit/", views.edit_page, name="edit"),
 ]
